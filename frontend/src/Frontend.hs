c14b086bfbc9c25f734e1a1b67b4503767513784
Verlon Smith <verlonsmith@Verlons-MacBook-Pro.local>
2019-05-08 20:39:21 -0600

    Created a basic button component.

---
+++ frontend/src/Frontend.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+module Frontend where
+
+import qualified Data.Text as T
+import Obelisk.Frontend
+import Obelisk.Route
+import Reflex.Dom.Core
+
+import Common.Api
+import Common.Route
+import Obelisk.Generated.Static
+
+import Frontend.Button --(myButton, Props, Variant(..), Size(..), Color(..))
+
+
+frontend :: Frontend (R FrontendRoute)
+frontend = Frontend
+  { _frontend_head = do
+      el "title" $ text "Obelisk Minimal Example with Reflex Components"
+      elAttr "link" ("href" =: "../../static/css/styles.css" <>
+                      "rel" =: "stylesheet" <>
+                      "type" =: "text/css"
+                      ) $ return ()
+  , _frontend_body = do
+      text "Welcome to Obelisk!"
+      el "p" $ text $ T.pack commonStuff
+      elAttr "img" ("src" =: static @"obelisk.jpg") blank
+      elAttr "h1" ("class" =: "test") $
+        el "div" $ text "This should be a child element"
+      el "ul" $ do
+        el "li" $ text "one"
+        el "li" $ text "two"
+        el "li" $ text "three"
+      myButton "Click Me!!" (Props Contained Primary False "" Small)
+      el "div" $ do
+        t <- inputElement def
+        dynText $ _inputElement_value t
+  }
---
+++ frontend/src/Frontend/Button.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Frontend.Button where
+
+import Reflex.Dom.Core
+import qualified Data.Text as T
+  
+data Variant
+  = Contained
+  | Text
+  | Outlined
+
+data Color
+  = Default
+  | Inherit
+  | Primary
+  | Secondary
+
+data Size
+  = Small
+  | Medium
+  | Large
+
+data Props = Props { variant  :: Variant
+                   , color    :: Color
+                   , disabled :: Bool
+                   , href     :: String
+                   , size     :: Size
+                   }
+myButton :: DomBuilder t m => String -> Props -> m ()
+myButton str props =
+  elAttr "button" ("href" =: (T.pack newHref) <>
+                                      "class" =: T.pack (
+                                        newVariant ++
+                                        " " ++
+                                        newColor ++
+                                        " " ++
+                                        newDisabled ++
+                                        " " ++
+                                        newSize
+                                        )
+                                      ) $ text (T.pack str)
+  where
+    newVariant  = case (variant props) of
+                    Contained -> "contained"
+                    Text -> "text"
+                    Outlined -> "outlined"
+    newColor    = case (color props) of
+                    Default -> "default"
+                    Inherit -> "inherit"
+                    Primary -> "primary"
+                    Secondary -> "secondary"
+    newDisabled = case (disabled props) of
+                    True -> "disabled"
+                    False -> ""
+    newHref     = case (href props) of
+                    val -> val
+    newSize     = case (size props) of
+                    Small -> "small"
+                    Medium -> "medium"
+                    Large -> "large"
+
